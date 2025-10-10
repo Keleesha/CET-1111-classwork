a8c9a2612a955cc6a4c78497d0abfec6dfc9c852
Keleesha <keleeshalowe1@gmail.com>
2025-10-10 18:12:07 -0400

    if-else practice

---
+++ days of the week.py
@@ -0,0 +1,28 @@
+
+num = int(input("Please enter a number from 1-7."))
+
+if num == 1:
+    print(" It's Monday, time to get back to work!")
+    
+if num ==2:
+    print("Its Tuesday.")
+
+
+if num ==3:
+    print("It's Wednesday.")
+    
+if num ==4:
+    print("It's Thursday")
+    
+if num ==5:
+    print("It's Friday, the weekend is near!")
+    
+if num ==6:
+    print("It's Saturday, the weekend is here!")
+    
+if num ==7:
+    print("Its Sunday")
+
+else:
+    print("You need not enter a number in the appropriate range.")
+
