33f7ef132fc6747f0f3818a96e7b585c18e04338
Keleesha <keleeshalowe1@gmail.com>
2025-11-12 08:46:41 -0500

    if-elif-else

---
+++ Age classification.py
@@ -0,0 +1,13 @@
+age = int(input("What is your age?"))
+
+if age <= 1:
+    print("You are a infant.")
+
+elif 13> age >1:
+    print("You are a child.")
+
+elif 20> age >=13:
+    print("You are a teenager.")
+
+else:
+    print("You are an adult.")
